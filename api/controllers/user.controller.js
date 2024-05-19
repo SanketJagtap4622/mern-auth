6e9f2f63f4b3c23c080d9043f99fc0165d9cfc4e
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-05-19 19:50:47 +0530

    complete update user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -27,7 +27,7 @@ export const updateUser = async (req, res, next) => {
           username: req.body.username,
           email: req.body.email,
           password: req.body.password,
-          profilePictue: req.body.profilePictue,
+          profilePicture: req.body.profilePicture,
         },
       },
       { new: true }
